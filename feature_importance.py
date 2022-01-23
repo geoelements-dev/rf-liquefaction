859bc4afc7a9025e6b7ac307f7245690f7be02bb
chhsiao93 <chhsiao@utexas.edu>
2022-01-23 17:07:50 -0600

    Revised code
    add comments in the function.
    add new argument for axis limit.
    modify the path and model names in main.py.

--- feature_importance.py
+++ feature_importance.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
 import numpy as np
 import pickle
 import pandas as pd
@@ -10,28 +6,49 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator, PercentFormatter
 
 
-def plot_importances(models, model_names, labels=None):
-    #models: an model object or a list of model objects. 
-    #model_names: a string or a list of strings corresponding to the models.
-    #labels: a list of strings. The label order shown in x axis.
+def plot_importances(models, model_names, labels=None, y_limit=(0,0.5)):
+
+    """Plot feature importances of random forest models.
+
+    Args:
+        models: Model object or a list of model objects.
+        model_names: Model name or a list of name for each model.
+        labels: A list of feature name that x labels show in this order. 
+        y_limit: The limit of y axis. 
+
+    Return:
+        fig: A matplotlib.figure.Figure object.
+    """
+
     if isinstance(models, RandomForestClassifier):
         models = [models]
         model_names = [model_names]
         
     if labels is None:
         labels = np.unique(np.concatenate([model.feature_names_in_ for model in models]))
-                           
+    
+    # X ticks position                       
     x = np.arange(len(labels))
+
+    # Number of models
     model_count = len(models)
-    width=1/model_count-0.05
+
+    # Width of bars
+    width = 1/model_count-0.05
+
+    # Make the center of the bar groups align with the tick
+    offset = 0.5
+
+    # create a figure and an axes objects
     fig,ax = plt.subplots()
-    offset=0.5
+
                            
     for model, model_name in zip(models, model_names):
 
         importances = model.feature_importances_
         feature_names = model.feature_names_in_
-        
+
+        #Create an array of the coordinates where bars should plot.
         coordinate = np.array([np.where([i==j for i in labels]) for j in feature_names]).reshape(-1)
         rects = ax.bar(coordinate-(model_count/2-offset)*width, importances,width,label=model_name)
         offset+=1
@@ -39,7 +56,7 @@ def plot_importances(models, model_names, labels=None):
     ax.grid(axis='y')
     ax.set_xticks(x)
     ax.set_xticklabels(labels,rotation=45)
-    ax.set_ylim(0,0.5)
+    ax.set_ylim(y_limit)
     ax.set_ylabel('Relative Importance')
     ax.yaxis.set_minor_locator(MultipleLocator(5))
     ax.yaxis.set_major_formatter(PercentFormatter(1))
@@ -47,5 +64,4 @@ def plot_importances(models, model_names, labels=None):
     ax.legend()
     plt.show()
 
-    #return a matplotlib.figure.Figure object, in case you want to save the figure.
     return fig
--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
 import pickle
 from sklearn.ensemble import RandomForestClassifier
 from feature_importance import plot_importances
 
-path = './PRJ-2998/Model Usage/'
-model_names = ['RF_YN_Model0','RF_YN_Model3','RF_YN_Model5','RF_Displ_Model0','RF_Displ_Model3','RF_Displ_Model5']
+path = './PathToFile/'
+model_names = ['model_name1', 'model_name2', 'model_name3']
 models = [pickle.load(open(path+model_name + '.pkl', 'rb')) for model_name in model_names]
 
 fig = plot_importances(models,model_names)
