c28c402486c79d576ae957e37aafba8310091a5c
chhsiao93 <chhsiao@utexas.edu>
2022-01-23 22:13:10 -0600

    path variable change
    Change path variable into an argparse argument.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import argparse
 import pickle
 from sklearn.ensemble import RandomForestClassifier
 from feature_importance import plot_importances
 
-path = './PathToFile/'
+parser = argparse.ArgumentParser()
+parser.add_argument("path", help="Input a directory path where the models are.")
+args = parser.parse_args()
+
 model_names = ['model_name1', 'model_name2', 'model_name3']
-models = [pickle.load(open(path+model_name + '.pkl', 'rb')) for model_name in model_names]
+models = [pickle.load(open(args.path+'/'+model_name + '.pkl', 'rb')) for model_name in model_names]
 
 fig = plot_importances(models,model_names)
 #fig.savefig('figure_name.png')
