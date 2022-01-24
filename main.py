a69ff305bfd2c03af5f2faa2645324137563b559
chhsiao93 <chhsiao@utexas.edu>
2022-01-24 13:05:13 -0600

    Add argparse
    Add nargs for model names.
    Add an optional args -s for saving the figure.

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from feature_importance import plot_importances
 
 parser = argparse.ArgumentParser()
 parser.add_argument("path", help="Input a directory path where the models are.")
+parser.add_argument("model_names", nargs='+', type=str, help="Input model names")
+parser.add_argument("-s", "--save", help="Input the name of figure")
 args = parser.parse_args()
 
-model_names = ['model_name1', 'model_name2', 'model_name3']
-models = [pickle.load(open(args.path+'/'+model_name + '.pkl', 'rb')) for model_name in model_names]
+models = [pickle.load(open(args.path+'/'+model_name + '.pkl', 'rb')) for model_name in args.model_names]
+fig = plot_importances(models,args.model_names)
 
-fig = plot_importances(models,model_names)
-#fig.savefig('figure_name.png')
+if args.save is not None:
+	fig.savefig(args.save+'.png')
